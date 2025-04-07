4412827a549a604ca8f040bc61829068206969c5
istifano <elamiriamir7@gmail.com>
2025-04-07 18:57:18 +0200

    Ajouter banner sur home page

--- src/components/section/HeroSection.jsx
+++ src/components/section/HeroSection.jsx
@@ -5,16 +5,20 @@ import Shop from '../button/Shop';
 
 const HeroSection = () => {
   return (
-    <div className="relative h-screen w-full overflow-hidden ">
-      <video src={video} autoPlay muted loop className="absolute inset-0 w-full h-full object-cover" >sqs</video>
-        <div className="relative z-10 flex flex-col items-center justify-center h-full text-center text-white bg-black bg-opacity-40 px-4">
+    <div className="relative h-screen w-full overflow-hidden">
+      {/* Video Background */}
+      <video autoPlay loop muted className="absolute top-0 left-0 w-full h-full object-cover z-0">
+        <source src={video} type="video/mp4" />
+      </video>
+
+      <div className="absolute top-0 left-0 w-full h-full  bg-opacity-40 z-10 flex flex-col items-center justify-center text-center text-white px-4">
         <h1 className="text-5xl md:text-6xl font-bold tracking-wider">
           MASTER YOUR GAME
         </h1>
-        
+
         <div className="mt-4 text-4xl md:text-5xl font-bold text-yellow-500">
           <ReactTyped
-            strings={["SNOOKER SITE Aamir","SNOOKER SITE El Amiri"]}
+            strings={["SNOOKER SITE Aamir", "SNOOKER SITE El Amiri"]}
             typeSpeed={100}
             backSpeed={50}
             loop
@@ -22,12 +26,12 @@ const HeroSection = () => {
             cursorChar="|"
           />
         </div>
-        
-        <p className="mt-6 text-xl mb-10">Built For Snooker Enthusiasts!     </p>
-        
+
+        <p className="mt-6 text-xl mb-10">Built For Snooker Enthusiasts!</p>
+
         <div className="flex flex-col sm:flex-row justify-center gap-4 mt-4">
           <Shop />
-          
+
           <button className="px-8 py-3 bg-white text-black font-bold rounded-full hover:bg-gray-200 transition duration-300">
             SNOOKER NEWS AND RESULTS
           </button>
